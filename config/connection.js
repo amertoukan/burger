f6a78c70bc5123823a8d0b8d6337a5b42530ff16
John Doe <toukan.amer@gmail.com>
2018-09-06 20:12:56 -0400

    JawsDB_URL

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 require ('dotenv').config();
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var connection ;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection (process.env.JAWSDB_URL) 
+} else {  ({
     host: 'localhost',
     port: 8889,
     user: process.env.user,
     password: process.env.password,
     database : 'burgers_db'
 });
+}
 
 connection.connect (function(err){
     if (err){
